22114841486b7caf8ed65815a138bf5e7ff64527
sumirart <akun.pratamasumirat@gmail.com>
2019-06-26 15:49:31 +0700

    get single id

--- src/main/java/com/sumirart/rest/controller/StudentsController.java
+++ src/main/java/com/sumirart/rest/controller/StudentsController.java
@@ -3,6 +3,8 @@ package com.sumirart.rest.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,7 @@ public class StudentsController {
 	
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping()
+	@GetMapping()
 	public StudentsModel get(
 			@RequestParam String name, @RequestParam Integer age, @RequestParam String address,
 			@RequestParam String school) {
@@ -32,8 +34,20 @@ public class StudentsController {
 		return students;
 	}
 	
+	@GetMapping("/{itemId}")
+	public StudentsModel get(@PathVariable(name="itemId", required=true) Integer itemId) {
+		StudentsModel students = new StudentsModel();
+		students.setModel(itemId, "null", 0, "null", "null"
+//				caseUtils.uppercase(name), 
+//				age, 
+//				caseUtils.uppercase(address), 
+//				caseUtils.uppercase(school));
+				);
+				
+		return students;
+	}
 	
-	@RequestMapping("/sample_utils")
+	@GetMapping("/sample_utils")
 	public String contohUtils(@RequestParam("input") String input) {
 		return caseUtils.uppercase(input);
 	}
